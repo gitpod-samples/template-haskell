7bff74bfec81bc78f93f3a6107ca20d3726de34e
Geoffrey Huntley <ghuntley@ghuntley.com>
2021-07-06 19:39:09 +1000

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main (main) where
+
+import GitpodTemplate (someFunc)
+
+
+main :: IO ()
+main = someFunc
---
+++ src/GitpodTemplate.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2021 Gitpod
+SPDX-License-Identifier: MIT
+Maintainer: Gitpod <contact@gitpod.io>
+
+See README for more info
+-}
+
+module GitpodTemplate
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
